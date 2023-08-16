55bd62b4ef1518f520730c38656512cc727c1bd1
UJJWAL GUPTA <ujjwal.gupta.phy22@itbhu.ac.in>
2023-08-16 20:30:40 +0530

    Added slideshow

--- components/Home/alums.js
+++ components/Home/alums.js
@@ -1,14 +1,15 @@
 'use client'
 import React from 'react'
 import classes from './imageSlider.module.css';
+import img1 from "@/public/home/image1.jpg"
+import Image from 'next/image'
 import {ImageSlider} from './image_slider_component';
 const Alums = () => {
    return (
-        <div className="min-h-screen navbar-hero w-full">
-           <h4 className={classes.ouralums}>Our Alums</h4>
-           <div className={classes.imageContainer}>
-            <ImageSlider></ImageSlider>
-           </div>
+        <div className={classes.imagerScroller}>
+           <h1 className={classes.ouralums}></h1>
+           {/* <ImageSlider></ImageSlider> */}
+          <ImageSlider></ImageSlider>
         </div>
     )
 }
--- components/Home/image_slider_component.js
+++ components/Home/image_slider_component.js
@@ -9,45 +9,22 @@ import img2 from "@/public/home/image2.webp"
 import img3 from "@/public/home/image3.webp"
 // this section contains all the refs to images to be displayes in alums sections
 import Image from 'next/image'
-import  { useState,useEffect } from 'react';
+
 
 let image_url_container=[img1,img2,img3]
 function ImageSlider() {
-    const [currentIndex, setCurrentIndex] = useState(0);
-  
-    const goToPrevious = () => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === 0 ? image_url_container.length - 1 : prevIndex - 1
-      );
-    };
-  
-    const goToNext = () => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === image_url_container.length - 1 ? 0 : prevIndex + 1
-      );
-    };
-  
-    useEffect(() => {
-      const interval = setInterval(goToNext, 3000);
-  
-      return () => clearInterval(interval);
-    }, []);
+   let image_container=image_url_container.map((item,pos)=>{
+    return(
+      <section class={classes.scroll_item} ><Image
+      src={item}
+      className={classes.macs_images}
+    /></section>
+    )
+   })
   
     return (
-      <div className={classes.image_holder}>
-        <Image
-          src={image_url_container[currentIndex]}
-          alt={`Slide ${currentIndex + 1}`}
-          className={classes.slider_container}
-        />
-        <div className={classes.slider_controls}>
-          <div><button onClick={goToPrevious} className={`${classes.slider_btns} ${classes.btn1}`}>
-          &lt;
-          </button></div>
-         <div> <button onClick={goToNext} className={`${classes.slider_btns} ${classes.btn2}`}>
-         &gt;
-          </button></div>
-        </div>
+      <div className={classes.scroll_container}>
+       {image_container}
       </div>
     );
   }
