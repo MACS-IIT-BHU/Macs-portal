74c283e747e44677971bddd6b9f44f9def3f9aa7
aaravm <aarav.mehta.mat22@itbhu.ac.in>
2023-08-17 18:16:34 +0530

    added our team, removed style, and added tailwind instead

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,26 @@
 import React from 'react'
-// import { _UilFacebookF } from '@iconscout/react-unicons'
+import Our_team from './Home/our_team'
+
 const Footer = () => {
     return (
-        <div className='bg-[#1E1E1E] min-h-[300px] flex flex-col gap-4 '>
+        <div className='bg-[#1E1E1E] min-h-[300px] flex flex-col gap-4 p-5'>
+            <div>
+                <div className='text-white text-4xl text-center m-5'>Our Team</div>
+                <div className="grid grid-cols-5 gap-4">
+                    <div><Our_team title="Prof. Sanjay Kumar Pandey" img="https://iitbhu.ac.in/contents/mat/img/people/mat_faculty_sanjay.jpg"/></div>
+                    <div><Our_team title="Dr. Abhash Kumar Jha" img="https://iitbhu.ac.in/contents/mat/img/people/mat_faculty_abhash.jpg"/></div>
+                    <div><Our_team title="Dr. Sunil Kumar" img="https://iitbhu.ac.in/contents/mat/img/people/mat_faculty_sunil.jpg"/></div>
+                    <div><Our_team title="Dr. Anoop Singh" img="https://iitbhu.ac.in/sites/default/files/mat/img/people/mat_faculty_anoop.jpg"/></div>
+                    <div><Our_team title="Dr. Amit Kumar" img="https://iitbhu.ac.in/sites/default/files/mat/img/people/mat_faculty_amitkr.jpg"/></div>
+                </div>
+
+                
+            </div>
+            <div className='flex flex-col items-center'>
+            
+                </div>
             <div className="grid grid-cols-3 gap-4">
                 <div className='mx-auto py-5'>
-                    {/* <img src="https://s3-alpha-sig.figma.com/img/f9e4/4697/f53ee055bd23e54abfaeba247328c934?Expires=1692576000&Signature=KhTIzdZbP2NypCb-9xJYSkcIH2hzv7twKqLm90cwrFiV3IFwbTXCFNwRJVVPlZTNBIaOOefA7kd22ctk3tEFsQcBuFNkYECeR0wliueZRcwtcqONMflYRQtLkxRPoSrBUkGitvCn-lRVqs-APpkKHtCFHYvYZRVs9AmPFwLTTFM0Z83fvDJ6H8~Vi759rLcLhElBtMETspYXi9twDyU8ZF1YgYE2065CEZxzppAEatqz6~8-kXSbbZg4EEgh70-I22KOad3BITBectmTDmqsg52Of0~hfJNAPH5cveDBV0YjRy4isLrE8eXUJ0CaYBEvdv7VgJuSJ82TxxfmuDFK~g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" ></img> */}
                     </div>
                 <div className="flex flex-col space-x-4 space-y-3 mx-auto py-20">
 
@@ -15,25 +30,11 @@ const Footer = () => {
                         <div className='containimg'><a href="#"><svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24" id="instagram"><path fill="#FFFFFF" d="M17.34,5.46h0a1.2,1.2,0,1,0,1.2,1.2A1.2,1.2,0,0,0,17.34,5.46Zm4.6,2.42a7.59,7.59,0,0,0-.46-2.43,4.94,4.94,0,0,0-1.16-1.77,4.7,4.7,0,0,0-1.77-1.15,7.3,7.3,0,0,0-2.43-.47C15.06,2,14.72,2,12,2s-3.06,0-4.12.06a7.3,7.3,0,0,0-2.43.47A4.78,4.78,0,0,0,3.68,3.68,4.7,4.7,0,0,0,2.53,5.45a7.3,7.3,0,0,0-.47,2.43C2,8.94,2,9.28,2,12s0,3.06.06,4.12a7.3,7.3,0,0,0,.47,2.43,4.7,4.7,0,0,0,1.15,1.77,4.78,4.78,0,0,0,1.77,1.15,7.3,7.3,0,0,0,2.43.47C8.94,22,9.28,22,12,22s3.06,0,4.12-.06a7.3,7.3,0,0,0,2.43-.47,4.7,4.7,0,0,0,1.77-1.15,4.85,4.85,0,0,0,1.16-1.77,7.59,7.59,0,0,0,.46-2.43c0-1.06.06-1.4.06-4.12S22,8.94,21.94,7.88ZM20.14,16a5.61,5.61,0,0,1-.34,1.86,3.06,3.06,0,0,1-.75,1.15,3.19,3.19,0,0,1-1.15.75,5.61,5.61,0,0,1-1.86.34c-1,.05-1.37.06-4,.06s-3,0-4-.06A5.73,5.73,0,0,1,6.1,19.8,3.27,3.27,0,0,1,5,19.05a3,3,0,0,1-.74-1.15A5.54,5.54,0,0,1,3.86,16c0-1-.06-1.37-.06-4s0-3,.06-4A5.54,5.54,0,0,1,4.21,6.1,3,3,0,0,1,5,5,3.14,3.14,0,0,1,6.1,4.2,5.73,5.73,0,0,1,8,3.86c1,0,1.37-.06,4-.06s3,0,4,.06a5.61,5.61,0,0,1,1.86.34A3.06,3.06,0,0,1,19.05,5,3.06,3.06,0,0,1,19.8,6.1,5.61,5.61,0,0,1,20.14,8c.05,1,.06,1.37.06,4S20.19,15,20.14,16ZM12,6.87A5.13,5.13,0,1,0,17.14,12,5.12,5.12,0,0,0,12,6.87Zm0,8.46A3.33,3.33,0,1,1,15.33,12,3.33,3.33,0,0,1,12,15.33Z"></path></svg></a></div>
                     </div>
                     
-                        <div className='flex mx-auto gap-2 text-white' style={{
-                            fontFamily: 'Montserrat',
-                            fontSize: '18px',
-                            fontStyle: 'normal',
-                            fontWeight: '400',
-                            lineHeight: '24.3px', /* 135% */
-
-                        }}>
-                            <a href="#">Contact us</a> - <a href="#">Support</a> - <a href="#">Services</a>
+                        <div className='flex mx-auto gap-2 text-white font-montserrat text-xl font-normal'>
+                            <a href="#">Contact us</a> | <a href="#">Support</a> | <a href="#">Services</a>
                         </div>
-                        <div className='flex mx-auto text-white gap-2 ' style={{
-                            fontFamily: 'Montserrat',
-                            fontSize: '18px',
-                            fontStyle: 'normal',
-                            fontWeight: '400',
-                            lineHeight: '24.3px', /* 135% */
-
-                        }}>
-                            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <a href="#">Join us</a> -
+                        <div className='flex mx-auto text-white gap-2 font-montserrat text-xl font-normal '>
+                            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <a href="#">Join us</a> |
                             <a href="#">Contribute</a>
                         </div>
 
@@ -42,9 +43,9 @@ const Footer = () => {
                 <div></div>
 
             </div>
-            <div className='flex flex-col gap-7 items-center'>
+            <div className='flex flex-col gap-7 items-center text-white'>
                 <hr style={{
-                    color: 'white',
+                    
                     backgroundColor: 'white',
                     height: 1,
                     width: '80%',
