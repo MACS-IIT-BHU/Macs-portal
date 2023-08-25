90dc2903bcded67801556923db4f08c1cbf0d171
Shivansh Bhatnagar <shivansh.bhatnagar.mat22@itbhu.ac.in>
2023-08-25 12:07:08 +0530

    Update page.js

--- app/announcements/page.js
+++ app/announcements/page.js
@@ -3,43 +3,49 @@ import React from 'react'
 import classes from '../../components/Home/announcements.css'
 import { useState } from 'react'
 import data from '../../public/database.json'
-import Image from 'next/image'
 
 const Announcements = () => {
     const [jsonData, setJsonData] = useState(data);
+    const [num, setNum] = useState(3);
 
+    const button_handle = () => {
+        setNum(num+3 < jsonData.announcements.length ? num+3: jsonData.announcements.length);
+        console.log(num)
+    }
     return (
         <section id="cd-timeline" class="cd-container">
-            {jsonData.announcements.map((element) => (
+                    {jsonData.announcements.map((element) => (
+                        <div class="cd-timeline-block">
+                            {element.id <= num && <div>
+                            <div class="cd-timeline-img cd-picture">
+                                <img src="https://www.svgrepo.com/show/493649/circle-filled-circle-radio-filled-round-bullet.svg" alt="Picture"></img>
+                            </div> 
 
-                <div class="cd-timeline-block" key={element.id}>
-                    <div class="cd-timeline-img cd-picture">
-                        <Image src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-picture.svg" height="20" width="20" alt="Picture" />
-                    </div>
+                            <div class="cd-timeline-content">
+                                <div classname='header'><h2>{element.title}</h2></div>
+                                <p>{element.body.substr(0,101) + "..."}<a href = "#open-modal">Read More</a></p>
+                                {/* <a class="btn" href="#open-modal">Read More</a> */}
+                                <span class="cd-date">{element.date}</span>
+                            </div>
+                            <div id="open-modal" class="modal-window">
+                                <div>
+                                    <a href="#" title="Close" class="modal-close"><img src="https://img.icons8.com/?size=512&id=7703&format=png" className="cross" alt='close'></img></a>
+                                    <h1>{element.title}</h1>
+                                    <div className='modal-body'>{element.body}</div>
+                                </div>
+                            </div>
+                            </div>}
+                        </div>))}
 
-                    <div class="cd-timeline-content">
-                        <div classname='header'><h2>{element.title}</h2></div>
-                        <p>{element.minibody}</p>
-                        <a class="btn" href="#open-modal">Read More</a>
-                        <span class="cd-date">{element.date}</span>
-                    </div>
-                </div>))}
+                        <div class="cd-timeline-block">
+                            <button type='submit' onClick = {button_handle}>
+                                <div class="cd-timeline-img cd-picture">
+                                    <img src="https://www.svgrepo.com/show/80156/down-arrow.svg" alt="Picture"></img>
+                                </div>
+                            </button>
+                        </div>
 
-            <div id="open-modal" class="modal-window">
-                <div>
-                    <a href="#" title="Close" class="modal-close">Close</a>
-                    <h1>Exploratory Project Guide Allotment</h1>
-                    <div>Dear Students,
-                        Please provide your choices of supervisor for your exploratory project in the google form.
-                        Below is the link to google form: https://forms.gle/H8Pum42rxWxhGB666
-                        The deadline to submit your choices is July 18, 2023.
-                        With Regards,
-                        Convener DUGC
-                        Department of Mathematical Sciences,
-                        IIT(BHU) Varanasi - 221005
-                    </div>
-                </div>
-            </div>
+                           
         </section>
     );
 }
