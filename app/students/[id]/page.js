e0775947a1ae7624dfcff169abc0ee5f4591ff27
Anurag <anurag.kamboj.mat21@itbhu.ac.in>
2023-12-28 14:36:55 +0530

    sddjk

--- app/students/[id]/page.js
+++ app/students/[id]/page.js
@@ -14,9 +14,7 @@ const UserProfilePage = ({ params }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(
-          `http://localhost:3000/api/users/me?user=${params.id}`
-        );
+        const res = await axios.get(`/api/users/me?user=${params.id}`);
         setUser(res.data.data);
       } catch (error) {
         console.log(error);
