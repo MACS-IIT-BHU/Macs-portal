46109baa42ecd7aa0add2745d0ff82f093f9e88c
UJJWAL GUPTA <ujjwal.gupta.phy22@itbhu.ac.in>
2024-02-07 23:12:22 +0530

    changed the slider activity

--- components/Home/image_slider_component.js
+++ components/Home/image_slider_component.js
@@ -1,5 +1,5 @@
 'use client'
-import React from "react";
+import React,{useState,useEffect,useRef} from "react";
 import classes from './imageSlider.module.css';
 import Image from 'next/image'
 
@@ -10,8 +10,9 @@ import i2 from "@/public/home/image2.jpg"
 // this section contains all the refs to images to be displayes in alums sections
  
 
-// let image_url_container=[i2,i2,i2,i2,i2]
+//let image_url_container=[i2,i2,i2,i2,i2]
 function ImageSlider() {
+  let scrollContainerRef=useRef(null);
   const [activeIndex, setActiveIndex] = React.useState(0);
 
   const image_url_container = [i2, i2, i2,i2,i2];
@@ -33,6 +34,30 @@ function ImageSlider() {
     ));
   };
 
+  useEffect(() => {
+    // Set up a timer to scroll automatically every 3 seconds (adjust as needed)
+    const scrollInterval = setInterval(() => {
+      // Assuming `scrollContainerRef` is a reference to the element you're scrolling
+      const scrollContainer = scrollContainerRef.current;
+      const imageWidth = scrollContainer.offsetWidth;
+
+      // Calculate the new index for automatic scrolling
+      const newIndex = (activeIndex + 1) % image_url_container.length; // Adjust numberOfImages accordingly
+
+      // Update the active index and scroll to the next position
+      setActiveIndex(newIndex);
+      scrollContainer.scrollTo({
+        left: newIndex * ( ( imageWidth)-( imageWidth*0.1)),
+        behavior: 'smooth', // Use 'auto' for instant scroll
+      });
+    }, 3000); // Adjust the interval in milliseconds as needed
+
+    return () => {
+      // Clean up the interval when the component is unmounted
+      clearInterval(scrollInterval);
+    };
+  }, [activeIndex]);
+
   const image_container = image_url_container.map((item, pos) => {
     return (
       <section className={classes.scroll_item} key={pos}>
@@ -41,9 +66,11 @@ function ImageSlider() {
     );
   });
 
+
   return (
     <div className={classes.slider_container}>
       <div
+      ref={scrollContainerRef} 
         className={classes.scroll_container}
         onScroll={handleScroll}
         // Add other necessary properties for scrolling
