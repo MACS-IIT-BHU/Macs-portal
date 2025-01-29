22760bcb1087d1d498016becbab16aac9254341b
s4kr3d-w0r1d <saksham.madan.oct20@gmail.com>
2025-01-29 22:46:36 +0530

    Added new team members and their photos

--- components/teams/TeamMember.jsx
+++ components/teams/TeamMember.jsx
@@ -2,68 +2,41 @@ import React from "react";
 import TeamName from "./TeamName";
 import Member from "./Member";
 import memImg from "../../public/home/pexels-pixabay-220453.jpg";
+import Sanyam from "../../public/home/Sanyam.jpg";
+import Divyansh from "../../public/home/Divyansh.jpg";
+
 
 function TeamMember() {
   return (
     <div className="box-border flex flex-col gap-10 pt-10 pb-20 items-center justify-between w-screen">
       <div className="flex w-full  flex-col  items-center justify-center gap-10 ">
         <TeamName team={"Core Team"} />
+        <div className="flex flex-col md:flex-row gap-10">
         <Member
-          name={"Atul Tyagi"}
+          name={"Kartik Mishra"}
           position={"Vice President"}
           image={memImg}
         />
-        <div className="grid md:grid-cols-2 md:flex-row gap-10">
-          <Member
-
-            name={"Gatla Sushmita"}
-            position={"General Secretary"}
-            image={memImg}
-          />
-          <Member
-            name={"Shivangi Nayak"}
-            position={"Student Advisor"}
-            image={memImg}
-          />
-          <Member
-            name={"Akshat Shah"}
-            position={"Jt. General Secretary"}
-            image={memImg}
-          />
-          <Member
-            name={"Kartik Malik"}
-            position={"Jt. General Secretary"}
-            image={memImg}
-          />
+        <Member
+          name={"Akshat Shah"}
+          position={"General Secretary"}
+          image={memImg}
+         />
         </div>
-      </div>
-      <div className="flex w-full  flex-col  items-center justify-center gap-10 ">
-        <TeamName team={"Resource & Development Team"} />
-        <div className="flex flex-col md:flex-row gap-10">
-          <Member
-            name={"Lakshya Vardhan Singh Rathore"}
-            position={"Secretary"}
-            image={memImg}
-          />
-          <Member
-            name={"Divyansh Sahu"}
-            position={"Jt. Secretary"}
-            image={memImg}
-          />
-        </div>
+        
       </div>
       <div className="flex w-full  flex-col  items-center justify-center gap-10 ">
         <TeamName team={"Events Team"} />
         <div className="flex flex-col md:flex-row gap-10">
           <Member
-            name={"Harsh Lalit Parmar"}
+            name={"Divyansh Sahu"}
             position={"Secretary"}
-            image={memImg}
+            image={Divyansh}
           />
           <Member
-            name={"Akshat Jain"}
+            name={"Sanyam Jain"}
             position={"Jt. Secretary"}
-            image={memImg}
+            image={Sanyam}
           />
         </div>
       </div>
@@ -71,12 +44,12 @@ function TeamMember() {
         <TeamName team={"Publications & Alumni Team"} />
         <div className="flex flex-col md:flex-row gap-10">
           <Member
-            name={"Lavish Bansal"}
+            name={"Chirag Goel"}
             position={"Secretary"}
             image={memImg}
           />
           <Member
-            name={"B Niranjan"}
+            name={"Pranithi S"}
             position={"Jt. Secretary"}
             image={memImg}
           />
