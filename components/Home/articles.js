955d68b1c18eba32a10e3b6cf61a9835609c91f4
enigmapulse <navneetkashyap2006@gmail.com>
2025-01-29 23:38:16 +0530

    Changed appearance of articles on home

--- components/Home/articles.js
+++ components/Home/articles.js
@@ -31,7 +31,7 @@ const Articles = () => {
                             <Image src={image2} className='w-[100%] rounded-lg' a></Image>
                         </div>
                         <div className=' h-[20px] '></div>
-                        <div className=' bg-gray-300 h-[300px] md:h-[650px] rounded-lg p-4 overflow-y-auto no-scrollbar' >the image is of hegec edch Mathematics and Computing Society (MACS) is a non-profit organization run by the students of The Department of Mathematical Sciences.
+                        <div className=' bg-cyan-100 h-[300px] md:h-[650px] rounded-lg p-4 overflow-y-auto no-scrollbar text-gray-800 shadow-lg' >the image is of hegec edch Mathematics and Computing Society (MACS) is a non-profit organization run by the students of The Department of Mathematical Sciences.
                             We serve as a platform for students interested in mathematics, computer science, and related fields to come together, explore their interests, and enhance their knowledge and skills.Mathematics and Computing Society (MACS) is a non-profit organization run by the students of The Department of Mathematical Sciences.
                             We serve as a platform for students interested in mathematics, computer science, and related fields to come together, explore their interests, and enhance their knowledge and skills.
                             We organize various events, workshops, seminars, coding competitions, hackathons, and guest lectures to promote learning, foster a se
@@ -41,26 +41,26 @@ const Articles = () => {
 
                 <div className=" mt-10 md:mt-0 lg:mt-0 xl:mt-0 2xl:mt-0 lg:w-[50vw] md:w-[50vw] sm:w-[80%] flex flex-wrap pl-4 gap-x-6 gap-y-16 items-center justify-around ">
                     {article_holder.map((article, pos) => (
-                        <div className="p-4 bg-slate-700 rounded-md" key={pos}>
+                        <div className="p-6 bg-cyan-100/40 rounded-md shadow-lg  border-amber-700 text-gray-900 w-full" key={pos}>
                             <div className=' flex flex-col gap-3 p-4'>
-                                <div className='text-[#146C94] text-2xl'>{article.heading}</div>
-                                <div className='text-[#146C94] text-sm'>{article.date}</div>
+                                <div className='text-cyan-800 text-2xl font-semibold border-b-2 border-cyan-600 pb-1'>{article.heading}</div>
+                                <div className='text-sm text-gray-600'>{article.date}</div>
                             </div>
-                            <div className='text-white p-4'>
-                                {article.content.substr(0, 150) + "... "}<a className='text-[#146C94] hover:underline hover:text-blue-600' href="/articles" >Read More</a>
+                            <div className='mt-4'>
+                                {article.content.substr(0, 150) + "... "}<a className='text-cyan-700 hover:underline hover:text-cyan-900' href="/articles" >Read More</a>
                             </div>
 
-                            <div className=' text-white text-xl p-4'>{article.author}
+                            <div className=' text-lg mt-4 font-medium text-gray-700'>{article.author}
                             </div>
                         </div>
                     ))
                     }
 
-                    <div className=" text-center bg-[rgb(255,255,255)] cursor-pointer rounded-lg " >
+                    <div className=" text-center bg-cyan-600 text-white cursor-pointer rounded-lg shadow-lg p-4 w-full max-w-xs " >
 
                         <div>
-                            <a href="/articles"> <button className='text-[#146C94] hover:underline pt-2 pb-2  w-[200px] h-[auto]  ' id='abcdef'>Click here for Articles &nbsp;</button>
-                                <button type="button" className="text-white bg-[#146C94] focus:ring-2 font-black rounded-full px-2 text-center mr-0 mb-0">&gt;</button></a>
+                            <a href="/articles"> <button className='text-white hover:underline pt-2 pb-2 w-full font-bold' id='abcdef'>Click here for Articles &nbsp;</button>
+                                <button type="button" className="bg-white text-cyan-600 focus:ring-2 font-black rounded-full px-2 text-center">&gt;</button></a>
                         </div>
                     </div>
                 </div>
